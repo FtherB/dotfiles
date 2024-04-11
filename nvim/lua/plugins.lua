231d2f6a525ae846df18d5461479ea48dd76c1fb
FtherB <aliclahso29103@gmail.com>
2024-04-11 13:27:28 +0900

    updated

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -68,6 +68,16 @@ require("lazy").setup({
       ft = { "markdown" },
       lazy = true,
     },
+
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons",
+            "MunifTanjim/nui.nvim",
+        },
+    },
 },
 -- lazy options
 {
