34743aeb09c8e35239eb602872f782233f7b1c77
FtherB <aliclahso29103@gmail.com>
2024-04-26 15:20:58 +0900

    updated

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -10,11 +10,8 @@ km.set('n','<leader>f', builtin.live_grep, {})
 km.set('n','<leader>fb', builtin.buffers, {})
 km.set('n','<leader>fh', builtin.help_tags, {})
 
-
 -- other systems
 -- split windows
-km.set('n','ss','<cmd>split<Return><C-w>w')
-km.set('n','sv','<cmd>vsplit<Return><C-w>w')
 -- move active window
 -- km.set('','sh','<C-w>h')
 -- km.set('','sk','<C-w>k')
