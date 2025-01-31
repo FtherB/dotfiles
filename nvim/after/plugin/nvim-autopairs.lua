7a71bdc430616b148269c622968d2a5d30211ec4
FtherB <aliclahso29103@gmail.com>
2025-01-31 18:07:30 +0900

    updated

--- nvim/after/plugin/nvim-autopairs.lua
+++ nvim/after/plugin/nvim-autopairs.lua
@@ -1,7 +1,7 @@
 local Rule = require('nvim-autopairs.rule')
 local npairs = require('nvim-autopairs')
 
-npairs.add_rule(Rule("「","」"))
-npairs.add_rule(Rule("（","）"))
-npairs.add_rule(Rule("『","』"))
-npairs.add_rule(Rule("【","】"))
+-- npairs.add_rule(Rule("「","」"))
+-- npairs.add_rule(Rule("（","）"))
+-- npairs.add_rule(Rule("『","』"))
+-- npairs.add_rule(Rule("【","】"))
