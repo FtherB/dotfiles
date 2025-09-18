df8bc49ef54b33a73f66882c728d4eb40faf4a55
FtherB <aliclahso29103@gmail.com>
2025-09-18 21:19:05 +0900

    reworked for mason v.2

--- nvim/lua/lsps.lua
+++ nvim/lua/lsps.lua
@@ -1,30 +1,9 @@
-local on_attach = function(client, bufnr)
-
-	local set = vim.keymap.set	
-	set('n','K','<cmd>lua vim.lsp.buf.hover()<CR>')
-	set('n','gf','<cmd>lua vim.lsp.buf.formatting()<CR>')
-	set('n','gr','<cmd>lua vim.lsp.buf.references()<CR>')
-	set('n','gd','<cmd>lua vim.lsp.buf.definition()<CR>')
-	set('n','gD','<cmd>lua vim.lsp.buf.declaration()<CR>')
-	set('n','gi','<cmd>lua vim.lsp.buf.implementation()<CR>')
-	set('n','gt','<cmd>lua vim.lsp.buf.type_definition()<CR>')
-	set('n','gn','<cmd>lua vim.lsp.buf.rename()<CR>')
-	set('n','ga','<cmd>lua vim.lsp.buf.code_action()<CR>')
-	set('n','ge','<cmd>lua vim.diagnostic.open_float()<CR>')
-	set('n','g]','<cmd>lua vim.diagnostic.goto_next()<CR>')
-	set('n','g[','<cmd>lua vim.diagnostic.goto_prev()<CR>')
-
-end
-
 require("mason").setup()
-require("mason-lspconfig").setup()
-require("mason-lspconfig").setup_handlers{
-    function (server_name)
-        require("lspconfig")[server_name].setup{
-            on_attach = on_attach
-        }
-    end,
-}
+
+require("mason-lspconfig").setup {}
+
+vim.lsp.enable(require('mason-lspconfig').get_installed_servers())
+
 --LSP handler
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {virtual_text = false })
 
