895248c127406dd780f7abad547398af69fc61a3
FtherB <aliclahso29103@gmail.com>
2026-02-09 13:57:23 +0900

    changed prompts

--- config/bash/common/prompt.bash
+++ config/bash/common/prompt.bash
@@ -1,3 +1,5 @@
+PS1_ARW=''
+
 function os_symbols() {
     if [ `uname` = Darwin ]; then
         echo ""
@@ -45,7 +47,7 @@ function show_venv() {
     [[ -x "$py" ]] || return 0
     ver="$("$py" -V 2>&1 | awk '{print $2}')"
 
-    printf ' (%s v%s) ' "$name" "$ver"
+    printf '(%s v%s)' "$name" "$ver"
 }
 
 function show_conda() {
@@ -53,10 +55,30 @@ function show_conda() {
         return
     fi
 
-    if [ -z "${VIRTUAL_ENV}" ]; then
-        printf ' (conda:%s)' "${CONDA_PROMPT_MODIFIER}"
+    printf '(conda:%s)' "${CONDA_PROMPT_MODIFIER}"
+}
+
+ESC_STR=$'\033'
+
+PS1_SP3_R="${ESC_STR}[38;2;59;118;240;48;2;0;79;122m${PS1_ARW}"
+PS1_SP4_R="${ESC_STR}[38;2;0;79;122;48;2;28;58;94m${PS1_ARW}"
+SKIP_PY_R="${ESC_STR}[38;2;59;118;240;48;2;28;58;94m${PS1_ARW}"
+PY_STY_R="${ESC_STR}[38;2;228;228;228;48;2;0;79;122m"
+
+function show_python() {
+    local conda venv
+
+    conda="$(show_conda)"
+    venv="$(show_venv)"
+
+    if [[ -n "$conda" && -n "$venv" ]]; then
+        printf '%s %s%s%s %s' "${PS1_SP3_R}" "${PY_STY_R}" "$conda" "$venv" "${PS1_SP4}"
+    elif [ -n "$conda" ]; then
+        printf '%s %s%s %s' "${PS1_SP3_R}" "${PY_STY_R}" "$conda" "${PS1_SP4_R}"
+    elif [ -n "$venv" ]; then
+        printf '%s %s%s %s' "${PS1_SP3_R}" "${PY_STY_R}" "$venv" "${PS1_SP4_R}"
     else
-        printf ' (conda:%s) ' "${CONDA_PROMPT_MODIFIER}"
+        printf '%s' "${SKIP_PY_R}"
     fi
 }
 
@@ -138,9 +160,8 @@ else
 fi
 
 # --- PS1 appearance ---
-PS1_ARW=''
-PS1_OS="\[\e[38;2;9;12;12;48;2;102;150;255m\] ${OSSYM} "
 PS1_SP0="\[\e[38;2;102;150;255;48;2;173;199;255m\]${PS1_ARW}"
+PS1_OS="\[\e[38;2;9;12;12;48;2;102;150;255m\] ${OSSYM} "
 
 PS1_HST="\[\e[38;2;28;58;82;48;2;173;199;255m\] \h${CONT} "
 PS1_SP1="\[\e[38;2;173;199;255;48;5;15m\]${PS1_ARW}"
@@ -149,10 +170,6 @@ PS1_USR="\[\e[38;2;28;58;82;48;5;15m\] \u "
 PS1_SP2="\[\e[38;5;15;48;2;59;118;240m\]${PS1_ARW}"
 
 PS1_DIR="\[\e[38;2;228;228;228;48;2;59;118;240m\] \w "
-PS1_SP3="\[\e[38;2;59;118;240;48;2;0;79;122m\]${PS1_ARW}"
-
-PS1_PY="\[\e[38;2;228;228;228;48;2;0;79;122m\]"
-PS1_SP4="\[\e[38;2;0;79;122;48;2;28;58;94m\]${PS1_ARW}"
 
 PS1_DUR="\[\e[38;2;141;251;210;48;2;28;58;94m\]"
 PS1_SP5="\[\e[38;2;28;58;94;49m\]${PS1_ARW}"
@@ -161,7 +178,7 @@ PS1_BREAK="\n"
 PS1_CMD="\[\e[0;36;49m\]\$ "
 PS1_RST="\[\e[0;39;49m\]"
 
-export PS1="$PS1_BREAK$PS1_OS$PS1_SP0$PS1_HST$PS1_SP1$PS1_USR$PS1_SP2$PS1_DIR$PS1_SP3\
-$PS1_PY\$(show_conda)\$(show_venv)\
-$PS1_SP4$PS1_DUR\$(timer_ps1)\
+export PS1="$PS1_BREAK$PS1_OS$PS1_SP0$PS1_HST$PS1_SP1$PS1_USR$PS1_SP2$PS1_DIR\
+\[\$(show_python)\]\
+$PS1_DUR\$(timer_ps1)\
 $PS1_SP5$PS1_BREAK$PS1_CMD$PS1_RST"
