85278dd50c6622e7fa1ab796a0b1ddd5c661af53
FtherB <aliclahso29103@gmail.com>
2026-02-20 18:11:40 +0900

    disabled starship

--- config/zsh/common/others.zsh
+++ config/zsh/common/others.zsh
@@ -2,8 +2,8 @@
 export LANG=en_US.UTF-8
 
 # starship
-export PATH="${HOME}/.local/starship:$PATH"
-eval "$(starship init zsh)"
+# export PATH="${HOME}/.local/starship:$PATH"
+# eval "$(starship init zsh)"
 
 # python
 if command -v pyenv 1>/dev/null 2>&1; then
