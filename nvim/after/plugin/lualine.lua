388cd48b65397631fae6ca3b149d447bdae011eb
FtherB <aliclahso29103@gmail.com>
2026-05-21 09:51:52 +0900

    changed left powerline symbol

--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -43,8 +43,8 @@ require('lualine').setup {
   options = {
     icons_enabled = true,
     theme = 'auto',
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
     disabled_filetypes = {
       statusline = {},
       winbar = {},
