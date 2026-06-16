3298bcb13027a1d78dbad954823675176a3d1d01
FtherB <aliclahso29103@gmail.com>
2026-06-16 20:12:59 +0900

    moved yvm

--- config/zsh/common/others.zsh
+++ config/zsh/common/others.zsh
@@ -8,3 +8,8 @@
 if command -v pyenv 1>/dev/null 2>&1; then
   eval "$(pyenv init -)"
 fi
+
+export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
+
+export YVM_DIR=$HOME/.yvm
+[ -r $YVM_DIR/yvm.sh ] && . $YVM_DIR/yvm.sh
